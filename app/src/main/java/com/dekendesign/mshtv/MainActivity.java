c459e9642351c264a058d4a4051f247d6ba21b75
Rory Deken <rory.deken@th2.com>
2022-12-20 15:37:08 -0500

    updated

--- app/src/main/java/com/dekendesign/mshtv/MainActivity.java
+++ app/src/main/java/com/dekendesign/mshtv/MainActivity.java
@@ -2,16 +2,8 @@ package com.dekendesign.mshtv;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
-
-import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 import com.dekendesign.mshtv.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
