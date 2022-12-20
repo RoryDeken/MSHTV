00dfa0e838d37ba98040ba5aaf7e89e3c9122d49
Rory Deken <rory.deken@th2.com>
2022-12-20 16:37:15 -0500

    updated to use network

--- app/src/main/java/com/dekendesign/mshtv/MainActivity.java
+++ app/src/main/java/com/dekendesign/mshtv/MainActivity.java
@@ -1,21 +1,52 @@
 package com.dekendesign.mshtv;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.textclassifier.TextLinks;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dekendesign.mshtv.databinding.ActivityMainBinding;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-
+    public static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        String url = "https://www.mshtv.com/shop?format=json-pretty";
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
 
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                try {
+                    if(response.isSuccessful()){
+                        Log.v(TAG, response.body().string());
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "IO Exception caught: ", e);
+                }
+            }
+        });
     }
 }
