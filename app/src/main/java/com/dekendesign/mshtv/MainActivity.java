1ee656b87cd0f0ed2a1f88595319e1af1c45ae8f
Rory Deken <rory.deken@th2.com>
2023-01-02 15:01:35 -0500

    updated files and added new class

--- app/src/main/java/com/dekendesign/mshtv/MainActivity.java
+++ app/src/main/java/com/dekendesign/mshtv/MainActivity.java
@@ -1,15 +1,23 @@
 package com.dekendesign.mshtv;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.textclassifier.TextLinks;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dekendesign.mshtv.databinding.ActivityMainBinding;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.net.NetworkInterface;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -21,32 +29,72 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     public static final String TAG = MainActivity.class.getSimpleName();
+    private JSONObject campInfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        String url = "https://www.mshtv.com/shop?format=json-pretty";
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url(url).build();
+        String url = getString(R.string.store_url);
 
-        Call call = client.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+        if (isNetworkavailable()) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url(url).build();
 
-            }
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
 
-            @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                try {
-                    if(response.isSuccessful()){
-                        Log.v(TAG, response.body().string());
-                    }
-                } catch (IOException e) {
-                    Log.e(TAG, "IO Exception caught: ", e);
                 }
-            }
-        });
+
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                    try {
+                        String jsonData = response.body().string();
+                        Log.v(TAG, jsonData);
+                        if (response.isSuccessful()) {
+                            campInfo = getCampPageInfo(jsonData);
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "IO Exception caught: ", e);
+                    } catch (JSONException e) {
+                        Log.e(TAG, "JSON Execption caught", e);
+                    }
+                }
+
+
+            });
+        }
+    }
+
+    private JSONObject getCampPageInfo(String jsonData) throws JSONException {
+        JSONObject pageInfo = new JSONObject(jsonData);
+        String mainContent = pageInfo.getString("website");
+        Log.i(TAG, "From json with main content: " + mainContent);
+        return pageInfo;
+    }
+
+    private boolean isNetworkavailable() {
+
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+        boolean isAvailable = false;
+        if(networkInfo != null && networkInfo.isConnected()){
+            isAvailable = true;
+        } else {
+            Toast.makeText(this, R.string.network_unavailable_message, Toast.LENGTH_LONG).show();
+        }
+        return isAvailable;
+
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 }
